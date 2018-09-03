cf6f1955c5cc5571cee2cb2054631e099710ab23
Mario <mstoyanov09@gmail.com>
2018-09-03 12:37:56 -0500

    made changes to output to make it look nicer

--- CS5405/hw1/Demo.java
+++ CS5405/hw1/Demo.java
@@ -1,14 +1,17 @@
-/* Mario Stoyanov *
- * CS5405 HW1     */
+/**
+	Mario Stoyanov
+	CS5405 - Java GUIs
+	Homework 1
+	*/
 
-package code; 
+//package code; 
 
 import java.util.Scanner;
 import java.lang.System;
 
 public class Demo {
 	public static void main(String args[])	{
-		System.out.println("\n\n  ~~Name Reading Program~~\n\n");
+		System.out.println("\n\n|*~*~*~*~* Name Reading Program *~*~*~*~*|\n\n");
 		System.out.println("Please enter the following information:\n");
 		Scanner input = new Scanner(System.in);
 
@@ -25,6 +28,6 @@ public class Demo {
 		System.out.printf("Your phone number is: %s \n", number);
 		System.out.printf("Your address is: %s \n", address);
 
-		System.out.println("\n  Thank you for using Name Reading Program  \n");
+		System.out.println("\n|*~*~*~*~* Thank you for using Name Reading Program *~*~*~*~*|\n");
 	}
 }
