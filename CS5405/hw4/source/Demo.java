2de389caaa34197b000b45c9af34ff06a8f86480
Mario <mstoyanov09@gmail.com>
2018-10-04 01:12:36 -0500

    changes with hard coded data

--- CS5405/hw4/source/Demo.java
+++ CS5405/hw4/source/Demo.java
@@ -1,8 +1,12 @@
 package code; 
 
+import java.io.File;
+import java.util.Scanner;
+
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.application.Application;
 
@@ -10,19 +14,24 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 import javafx.scene.text.*;
+
+import java.lang.Math;
 
 public class Demo extends Application{
     Pane pane1 = new Pane();
     Pane pane2 = new Pane();
     Pane pane3 = new Pane();
     Pane pane4 = new Pane();
+    Pane pane5 = new Pane();
     Pane root = new Pane();
 
     Scene scene;
     Stage stage;
 
-    public void start(Stage stage){
+    public void start(Stage stage) {
         //root.getChildren().addAll.(pane1, button1, b2, button3,)
         root = homePage();
         scene = new Scene(root,700,600,Color.GRAY);
@@ -30,37 +39,81 @@ public class Demo extends Application{
         stage.setScene(scene);
         stage.show();
     }
-    Text t1,t2,t3,t4;
+
+    Text t1,t2,t3,t4,t5,t6;
     Button b1, b2, b3, b4;
+    
+    int x1 = 200;
+    int y1 = 100; 
+    int r1 = 50;
+    int x2 = 225;
+    int y2 = 100;
+    int r2 = 20;
+    
+    Circle c1, c2; 
 
     public Pane homePage(){
+
         t1 = new Text(10,90, "Author Text");
         t1.setText("Homework 4 \nAuthor: Mario Stoyanov \nEmail: msfp6@mst.edu");
         t1.setFont(new Font(20));
+        t1.setFill(Color.BLUE);
         t1.setWrappingWidth(600);
         t1.setTextAlignment(TextAlignment.CENTER);
 
-        // t5 = new Text(10,100, "Another Test Text \n asdadsdasdasd \n asdasdd");
-        // t6 = new Text(10, 180, "\nLink: url address for reference to specific page and quotation");
-        
         t2 = new Text(0, 20, "Description Text");
-        t2.setFont(new Font(20));              
+        t2.setFont(new Font(20));
+        t2.setFill(Color.RED);              
         t2.setWrappingWidth(500);               
         t2.setText("Homework 4 should contain a four buttons.");
         
         t3 = new Text(20,40, "Description of Description");
         t3.setFont(new Font(15));
+        t3.setFill(Color.RED); 
         t3.setWrappingWidth(600);
         t3.setText("The Author button should have the author name and email. \nThe Description button should contain problem description \nThe Problems button should list any problems encountered \nThe Demo button should provide a demo of Circles");        
         
-
         t4 = new Text(0, 20, "I did not encounter any problems");
         t4.setFont(new Font(20));
+        t4.setFill(Color.GREEN);
         t4.setWrappingWidth(600);       
+
+        t5 = new Text(0, 20, "Circle Demo");
+        t5.setFont(new Font(20));
+        t5.setFill(Color.CYAN);
+        t5.setWrappingWidth(600);
+        
+        c1 = new Circle();
+        c1.setCenterX(x1);
+        c1.setCenterY(y1);
+        c1.setRadius(r1);
+        c1.setFill(Color.RED);
+        c1.setStroke(Color.BLUE);
+        c1.setStrokeWidth(3);
+
+        c2 = new Circle();
+        c2.setCenterX(x2);
+        c2.setCenterY(y2);
+        c2.setRadius(r2);
+        c2.setFill(Color.AQUA);
+        c2.setStroke(Color.BLUE);
+        c2.setStrokeWidth(3);
+
+        if (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) > r1 + r2) {
+            t6 = new Text(0, 40, "They are Disjoint");
+        } else if (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) == r1 + r2 ||
+                   Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) == Math.abs(r1 - r2)) {
+            t6 = new Text(0, 40, "They touch");
+        } else if (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) < Math.abs(r1 - r2)) {
+            t6 = new Text(0, 40, "One is inside the other");
+        } else {
+            t6 = new Text(0, 40, "They properly overlap");
+        }
 
         pane1.getChildren().addAll(t1);        
         pane2.getChildren().addAll(t2,t3);        
-        pane3.getChildren().addAll(t4);        
+        pane3.getChildren().addAll(t4);
+        pane5.getChildren().addAll(t5, t6, c1, c2);        
         pane4.getChildren().addAll(pane1);
 
         pane1.relocate(10,50);        
@@ -79,7 +132,8 @@ public class Demo extends Application{
         root.getChildren().addAll(b1, b2, b3, b4, pane4);        
         b1.setOnAction(ae->{pane4.getChildren().clear();pane4.getChildren().add(pane1);});        
         b2.setOnAction(ae->{pane4.getChildren().clear();pane4.getChildren().add(pane2);});        
-        b3.setOnAction(ae->{pane4.getChildren().clear();pane4.getChildren().add(pane3);});        
+        b3.setOnAction(ae->{pane4.getChildren().clear();pane4.getChildren().add(pane3);});
+        b4.setOnAction(ae->{pane4.getChildren().clear();pane4.getChildren().add(pane5);});        
         
         return root; 
     }
